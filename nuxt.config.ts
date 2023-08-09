642206b40521a8efeb8d7a8bd426733942b16458
Gaston <cutkillerone@gmail.com>
2023-08-09 18:07:42 -0400

    integrando nuxtlabs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["bootstrap/dist/css/bootstrap.min.css","@/assets/styles/main.scss"],
+  modules: ['@nuxthq/ui'],
+  colorMode: {
+    preference: 'light'
+  }
 })
