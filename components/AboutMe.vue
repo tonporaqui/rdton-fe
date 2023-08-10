46e9cd8df9b7c4a0b7c141264a2533dd90abef7e
Gaston <cutkillerone@gmail.com>
2023-08-10 15:34:44 -0400

    modificaciones ui ux

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,9 +1,23 @@
 <template>
-    <section>
-      <h2>Sobre mí</h2>
-      <p>
-        [Tu biografía y experiencia aquí]
-      </p>
-    </section>
-  </template>
+  <section class="bg-black text-white p-8 rounded-lg shadow-md">
+    <h2 class="text-2xl font-bold mb-4">Sobre mí</h2>
+    <p class="mb-4">
+      Ingeniero Informático especializado en Arquitectura de Software apasionado por la investigación y nuevas tecnologías. Experiencia en aplicar metodologías ágiles en proyectos de desarrollo. Actualmente cursando un Magíster en Gestión de Información y Telecomunicaciones en la Universidad Andrés Bello, interesado en contribuir al avance tecnológico.
+    </p>
+    <h3 class="text-xl font-semibold mb-2">Habilidades técnicas:</h3>
+    <ul class="list-disc pl-5 mb-4">
+      <li>Lenguajes de programación: PHP, JavaScript, Vue.js, Nuxtjs, Python, Node.js, Flutter, JAVA, C#, ASP.NET.</li>
+      <li>Versionado: Git, GitHub.</li>
+      <li>Integración de Sistemas: Restful, SOAP, OAuth 2.0, WebSocket.</li>
+      <li>Arquitectura y administración de servicios AWS: Elastic Beanstalk, CodePipeline, Route 53, Amazon WorkSpace, Docker.</li>
+      <li>Bases de datos: Amazon DocumentDB, SQL, MySQL, MongoDB, SQLlite.</li>
+      <li>Herramientas de gestión: Jira/Confluence, Microsoft 365 E3, Trello.</li>
+    </ul>
+    <p class="mb-4">
+      Experiencia en metodologías ágiles como Scrum y en la implementación de la norma ISO 33.000. Enfoque de liderazgo orientado al logro de objetivos y gestión de riesgos en proyectos de ingeniería de software. Destacado por habilidades de comunicación efectiva y trabajo en equipo. Siempre dispuesto a compartir conocimientos y aprender de otros profesionales.
+    </p>
+  </section>
+</template>
+
+
   
--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,28 @@
 <template>
-    <header>
-      <h1>Gaston Sepulveda</h1>
-      <p>Ingeniero Informático</p>
-    </header>
-  </template>
-  
-  <style scoped>
-  header {
-    background-color: #333;
-    color: white;
-    padding: 20px 0;
-    text-align: center;
-  }
-  </style>
-  
+  <header class="relative text-orange-100 text-center py-15 bg-gradient-to-r from-sky-500 to-indigo-500">
+    <img src="/foto-perfil.png" alt="Foto de perfil" class="profile-image mb-4">
+    <h1 class="text-3xl font-bold mb-4 z-10">Gaston Sepulveda</h1>
+    <p class="max-w-lg mx-auto mb-6 z-10">
+      Ingeniero Informático apasionado por la tecnología y la innovación. Especializado en Arquitectura de Software. Magíster en Gestión de Información y Telecomunicaciones. Liderazgo orientado a objetivos y proyectos ágiles.
+    </p>
+    <nav class="flex justify-center space-x-4 flex-wrap z-10">
+      <a href="#" class="text-white hover:text-gray-300">Inicio</a>
+      <a href="#" class="text-white hover:text-gray-300">Sobre mí</a>
+      <a href="#" class="text-white hover:text-gray-300">Habilidades</a>
+      <a href="#" class="text-white hover:text-gray-300">Proyectos</a>
+      <a href="#" class="text-white hover:text-gray-300">Contacto</a>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.profile-image {
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  margin: 0 auto;
+  display: block;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  z-index: 10;
+}
+</style>
