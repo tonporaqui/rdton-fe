7752d470f297633c6036e0a687fd0cd8179763b9
Gaston <cutkillerone@gmail.com>
2023-09-01 01:23:42 -0400

    Terminando de actualizar AboutMe.vue

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -8,34 +8,32 @@
 		>
 			Sobre mí
 		</h2>
-
-		<!-- Contenedor de las tarjetas con diseño de cuadrícula -->
 		<div class="flex justify-center">
 			<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
-				<!-- Iterar sobre el contenido del store para mostrar cada card -->
 				<div
 					v-for="(info, index) in aboutMeInfo"
 					:key="info.id"
-					class="transform transition hover:scale-100 p-4 bg-light-bg100 dark:bg-dark-bg300 rounded-lg shadow-md dark:shadow-dark-bg200"
+					class="transform transition hover:scale-100 p-4 bg-light-bg100 dark:bg-dark-bg300 rounded-lg shadow-md dark:shadow-dark-bg200 flex flex-col items-center"
 					:data-aos="getAosEffect(index)"
 					:data-aos-delay="(index + 1) * 100"
 				>
 					<NuxtImg
 						:src="info.image || 'https://stackdiary.com/140x100.png'"
 						alt="image.ref"
-						width="300"
-						height="169"
-						class="w-full h-48 object-cover rounded-t-lg"
+						width="256"
+						height="256"
+						class="object-cover rounded-t-lg mb-4"
 					/>
 					<h3
-						class="text-xl font-semibold text-center mb-4 mt-4 text-light-accent200 dark:text-dark-primary100"
+						class="text-xl font-semibold text-center mb-4 text-light-accent200 dark:text-dark-primary100"
 					>
 						{{ info.title }}
 					</h3>
 					<span
 						class="pl-2 leading-relaxed text-light-text200 dark:text-dark-text100"
-						>{{ info.description }}</span
 					>
+						{{ info.description }}
+					</span>
 				</div>
 			</div>
 		</div>
@@ -60,5 +58,5 @@ export default {
 </script>
 
 <style scoped>
-/* Puedes agregar estilos adicionales aquí si lo necesitas */
+/* Estilos */
 </style>
--- store/aboutMeInfo.js
+++ store/aboutMeInfo.js
@@ -15,14 +15,14 @@ export const useMainStore = defineStore({
 				id: 2,
 				title:
 					"Estudiante de Magíster en Gestión de Información y Telecomunicaciones",
-				image: "",
+				image: "/images/student-boy.png",
 				description:
 					"Aplicando metodologías ágiles en proyectos de desarrollo. Actualmente cursando un Magíster en Gestión de Información y Telecomunicaciones en la Universidad Andrés Bello, interesado en contribuir al avance tecnológico.",
 			},
 			{
 				id: 3,
 				title: "Entusiasta de la Innovación Tecnológica",
-				image: "",
+				image: "/images/inovation-technology.png",
 				description:
 					"Aplicación de metodologías ágiles en proyectos de desarrollo. Persiguiendo un Magíster en Gestión de Información y Telecomunicaciones en la Universidad Andrés Bello, con interés en impulsar el progreso tecnológico.",
 			},
@@ -30,7 +30,7 @@ export const useMainStore = defineStore({
 				id: 4,
 				title:
 					"Líder en Ingeniería de Software con Enfoque en Metodologías Ágiles",
-				image: "",
+				image: "/images/leader.png",
 				description:
 					"Liderando el logro de objetivos y la gestión de riesgos en proyectos de ingeniería de software. Habilidades sobresalientes en comunicación efectiva y trabajo en equipo. Siempre dispuesto a compartir conocimientos y aprender de otros profesionales.",
 			},
