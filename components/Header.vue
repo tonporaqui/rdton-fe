36433aa93c2bcc3ef2cdc98282ab64f0e20af62f
Gaston <cutkillerone@gmail.com>
2023-09-15 12:05:33 -0300

    ordenando el header title nav y boton

--- components/Header.vue
+++ components/Header.vue
@@ -10,72 +10,73 @@
 				<!-- Container for title, nav, and switch -->
 				<div class="container mx-auto flex items-center justify-between">
 					<!-- Title for larger screens -->
-					<h1
-						class="z-10 hidden text-3xl font-semibold text-light-accent200 dark:text-dark-accent200 md:block">
-						<span class="text-2xl font-light italic">{{ titleNamePage }}</span>
-						<span class="ml-2 font-bold">{{ titlePage }}</span>
-					</h1>
-					<div class="md:hidden">
-						<button
-							type="button"
-							class="flex px-2 pb-4 pt-2 text-light-accent200 hover:text-light-primary100 focus:outline-none dark:text-dark-accent200 dark:hover:text-dark-primary100"
-							@click="toggleNav">
-							<svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-								<path
-									v-if="isOpen"
-									fill-rule="evenodd"
-									d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
-								<path
-									v-if="!isOpen"
-									fill-rule="evenodd"
-									d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
-							</svg>
-						</button>
+					<div
+						class="flex items-center justify-between text-light-accent200 dark:text-dark-accent200">
+						<div class="ml-2 block">
+							<span class="">
+								{{ titleNamePage }}
+							</span>
+							<span class="ml-1 text-2xl font-bold">
+								{{ titlePage }}
+							</span>
+						</div>
+
+						<div class="md:hidden">
+							<button
+								type="button"
+								class="flex px-2 pb-4 pt-2 text-light-accent200 hover:text-light-primary100 focus:outline-none dark:text-dark-accent200 dark:hover:text-dark-primary100"
+								@click="toggleNav">
+								<svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+									<path
+										v-if="isOpen"
+										fill-rule="evenodd"
+										d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
+									<path
+										v-if="!isOpen"
+										fill-rule="evenodd"
+										d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
+								</svg>
+							</button>
+						</div>
+						<!-- Navigation -->
+						<nav
+							:class="isOpen ? 'flex flex-col' : 'hidden'"
+							class="flex px-2 pb-4 pt-2 md:flex md:p-0 lg:z-10 lg:space-x-4">
+							<NuxtLink
+								:to="{ path: '/', hash: '#header-inicio' }"
+								class="mt-1 block rounded p-2 py-1 text-sm font-semibold text-light-accent200 transition-transform hover:scale-105 hover:bg-opacity-50 hover:text-light-primary100 dark:text-dark-accent200 dark:hover:text-dark-primary100">
+								Inicio
+							</NuxtLink>
+							<NuxtLink
+								:to="{ path: '/', hash: '#stack-section' }"
+								class="mt-1 block rounded p-2 py-1 text-sm font-semibold text-light-accent200 transition-transform hover:scale-105 hover:bg-opacity-50 hover:text-light-primary100 dark:text-dark-accent200 dark:hover:text-dark-primary100 sm:ml-2">
+								Habilidades
+							</NuxtLink>
+							<NuxtLink
+								:to="{ path: '/', hash: '#about-me-section' }"
+								class="mt-1 block rounded p-2 py-1 text-sm font-semibold text-light-accent200 transition-transform hover:scale-105 hover:bg-opacity-50 hover:text-light-primary100 dark:text-dark-accent200 dark:hover:text-dark-primary100 sm:ml-2">
+								Sobre mí
+							</NuxtLink>
+							<NuxtLink
+								:to="{ path: '/', hash: '#timeline-section' }"
+								class="mt-1 block rounded p-2 py-1 text-sm font-semibold text-light-accent200 transition-transform hover:bg-opacity-50 hover:text-light-primary100 first-letter:hover:scale-105 dark:text-dark-accent200 dark:hover:text-dark-primary100 sm:ml-2">
+								Mi línea de tiempo
+							</NuxtLink>
+							<NuxtLink
+								to="/biblioteca"
+								class="mt-1 block rounded p-2 py-1 text-sm font-semibold text-light-accent200 transition-transform hover:bg-opacity-50 hover:text-light-primary100 first-letter:hover:scale-105 dark:text-dark-accent200 dark:hover:text-dark-primary100 sm:ml-2">
+								Bibliotecla
+							</NuxtLink>
+						</nav>
+						<!-- ButtonSwitch -->
+						<div class="flex"></div>
+						<ButtonSwitch />
 					</div>
-					<!-- Navigation -->
-					<nav
-						:class="isOpen ? 'flex flex-col' : 'hidden'"
-						class="flex px-2 pb-4 pt-2 md:flex md:p-0 lg:z-10 lg:space-x-4">
-						<NuxtLink
-							:to="{ path: '/', hash: '#header-inicio' }"
-							class="mt-1 block rounded p-2 py-1 text-sm font-semibold text-light-accent200 transition-transform hover:scale-105 hover:bg-opacity-50 hover:text-light-primary100 dark:text-dark-accent200 dark:hover:text-dark-primary100">
-							Inicio
-						</NuxtLink>
-						<NuxtLink
-							:to="{ path: '/', hash: '#stack-section' }"
-							class="mt-1 block rounded p-2 py-1 text-sm font-semibold text-light-accent200 transition-transform hover:scale-105 hover:bg-opacity-50 hover:text-light-primary100 dark:text-dark-accent200 dark:hover:text-dark-primary100 sm:ml-2">
-							Habilidades
-						</NuxtLink>
-						<NuxtLink
-							:to="{ path: '/', hash: '#about-me-section' }"
-							class="mt-1 block rounded p-2 py-1 text-sm font-semibold text-light-accent200 transition-transform hover:scale-105 hover:bg-opacity-50 hover:text-light-primary100 dark:text-dark-accent200 dark:hover:text-dark-primary100 sm:ml-2">
-							Sobre mí
-						</NuxtLink>
-						<NuxtLink
-							:to="{ path: '/', hash: '#timeline-section' }"
-							class="mt-1 block rounded p-2 py-1 text-sm font-semibold text-light-accent200 transition-transform hover:bg-opacity-50 hover:text-light-primary100 first-letter:hover:scale-105 dark:text-dark-accent200 dark:hover:text-dark-primary100 sm:ml-2">
-							Mi línea de tiempo
-						</NuxtLink>
-						<NuxtLink
-							to="/biblioteca"
-							class="mt-1 block rounded p-2 py-1 text-sm font-semibold text-light-accent200 transition-transform hover:bg-opacity-50 hover:text-light-primary100 first-letter:hover:scale-105 dark:text-dark-accent200 dark:hover:text-dark-primary100 sm:ml-2">
-							Bibliotecla
-						</NuxtLink>
-					</nav>
-					<!-- ButtonSwitch -->
-					<div class="flex"></div>
-					<ButtonSwitch />
 				</div>
 			</div>
-			<!-- Title for smaller screens -->
-			<h1
-				class="z-10 mb-4 pt-14 text-center text-3xl text-light-accent200 dark:text-dark-primary200 sm:pt-56 md:hidden">
-				<span class="text-2xl font-light italic">{{ titleNamePage }}</span>
-				<span class="ml-2 font-bold">{{ titlePage }}</span>
-			</h1>
 			<!-- Profile Info -->
 			<figure
-				class="mx-auto max-w-3xl rounded-xl bg-light-bg100 py-2 shadow-md dark:bg-dark-bg300 dark:text-dark-text200 dark:shadow-dark-bg200 md:z-20 md:mt-32 md:flex md:p-0 lg:mt-40">
+				class="z-20 mx-auto my-12 max-w-3xl rounded-xl bg-light-bg100 py-2 shadow-md dark:bg-dark-bg300 dark:text-dark-text200 dark:shadow-dark-bg200 md:mt-32 md:flex md:p-0 lg:mt-40">
 				<nuxt-img
 					class="mx-auto h-28 w-28 rounded-full md:h-auto md:w-48 md:rounded-none"
 					src="/images/foto.jpg"
