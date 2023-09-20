2be46367e02f698760bf7bcd52c31218dfbe15e7
Gaston <cutkillerone@gmail.com>
2023-09-20 16:27:06 -0300

    dando animacion al modal

--- components/ModalCrudUser.vue
+++ components/ModalCrudUser.vue
@@ -1,7 +1,11 @@
 <template>
 	<div
-		v-if="isOpen"
-		class="fixed inset-0 z-50 flex items-center justify-center">
+		v-show="isOpen || isAnimatingOut"
+		class="fixed inset-0 z-50 flex items-center justify-center"
+		:class="{
+			'animate-jump-out opacity-0 delay-300 ease-out': isAnimatingOut,
+			'animate-jump-in opacity-100 ease-in': isOpen,
+		}">
 		<div class="rounded bg-white p-4 shadow-lg dark:bg-dark-bg200">
 			<h2 class="mb-4 text-xl text-light-text200 dark:text-dark-text100">
 				{{
@@ -33,7 +37,7 @@
 			</div>
 			<button
 				class="text-light-accent200 dark:text-dark-accent200"
-				@click="$emit('close')">
+				@click="closeWithAnimation">
 				Cancelar
 			</button>
 		</div>
@@ -50,13 +54,28 @@ interface Props {
 	userData: User | null
 }
 
+const isAnimatingOut = ref(false)
+
 const { isOpen, actionType, userData } = defineProps<Props>()
 
 const emits = defineEmits(['confirm', 'close'])
 
 const confirmAction = () => {
 	// Aquí puedes agregar validación de formulario para las acciones de crear y editar
-	emits('confirm', userData)
+
+	isAnimatingOut.value = true
+	setTimeout(() => {
+		emits('confirm', userData)
+		isAnimatingOut.value = false
+	}, 1000)
+}
+
+const closeWithAnimation = () => {
+	isAnimatingOut.value = true
+	setTimeout(() => {
+		emits('close')
+		isAnimatingOut.value = false
+	}, 1000) // Ajusta este valor al tiempo de duración de la animación de salida
 }
 </script>
 
